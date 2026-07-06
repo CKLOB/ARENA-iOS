643c6aa0fc3e0a332332a5edb9c1121c106c3bdb
김준표juunpy0 <junpyokim0106@gmail.com>
2026-07-07 08:27:35 +0900

    feat: Xcode 프로젝트 초기 설정

---
+++ ARENA/ARENAApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARENAApp.swift
+//  ARENA
+//
+//  Created by 김준표 on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ARENAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
